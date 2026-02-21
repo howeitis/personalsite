6791bce885753f10edaaf3170d4f338a98b4f04e
Owen Howe <Owen@Howe.app>
2026-02-20 22:31:46 -0500

    fix(build): Update vite.config.js base path to fix blank Vercel preview

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/personalsite/',
+  base: process.env.VERCEL ? '/' : '/personalsite/',
   plugins: [react()],
   test: {
     environment: 'jsdom',
